83c8cd5e93668e13b711ac4548393d3074b81328
etamar <etamar.levy@gmail.com>
2021-02-20 18:55:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader);
+    
+newHeader.setAttribute("id", "victory");
+    
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = 'Etamar ' + 'is the champion';
+document.body.appendChild(newHeader);
